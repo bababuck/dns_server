2919e9bc4e9e4b06caf15d1d2c1ea0964701037c
Ryan Buchner <rabuchner@ucdavis.edu>
2023-11-19 20:28:18 -0800

    Update .gitignore for emacs files

---
+++ server.c
@@ -0,0 +1,44 @@
+/**
+ * Base code for the server to interface with DNS requests.
+ *
+ * Use UDP protocol to communicate DNS responses.
+ */
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+typedef struct sockaddr_in sockaddr_in_t;
+typedef struct sockaddr sockaddr_t;
+
+void setup_server() {
+  int socket_info = socket(AF_INET, SOCK_DGRAM, 0); // UDP Socket
+  if (socket_info < 0) {
+    perror("socket");
+    exit(1);
+  }
+  sockaddr_in_t server;
+  server.sin_addr.s_addr = INADDR_ANY; // Any addresses
+  server.sin_family = AF_INET; // Internet Protocol v4 addresses
+  server.sin_port = 1053; // 53 is priviliged
+
+  // Bind socket to requested port
+  if (bind(socket_info, (sockaddr_t *) &server, sizeof(server)) < 0) {
+    perror("bind");
+    exit(2);
+  }
+
+  socklen_t length = sizeof(sockaddr_in_t);
+  if (getsockname(socket_info, (sockaddr_t *) &server, &length) < 0) {
+    perror("getsockname()");
+    exit(3);
+  }
+
+  printf("Port assigned: %d\n", server.sin_port);
+}
+
+int main(int argc, char *argv[]) {
+  setup_server();
+  return 0;
+}
