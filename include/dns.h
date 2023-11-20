ffd677432298d6e6df6cbc8122988713a6e394e0
Ryan Buchner <rabuchner@ucdavis.edu>
2023-11-19 20:28:39 -0800

    Work on dns a lil

--- include/dns.h
+++ include/dns.h
@@ -48,7 +48,26 @@ typedef struct {
 /**
  * Function to parse a raw DNS message.
  */
-message_t parse(const uint64_t raw_message[8]);
+bool parse_message(const uint8_t raw_message[8], message_t *message);
+
+bool parse_header(const uint8_t raw_message[8], header_t *header) {
+  header->id = ((uint16_t) raw_message][1]) & (((uint16_t) raw_message[0]) << 8);
+  header->qr = raw_message[2] & 1;
+if (header->qr) {
+  return false;
+  }
+  uint8_t opcode : 4;
+  uint8_t aa : 1;
+  uint8_t tc : 1;
+  uint8_t rd : 1;
+  uint8_t ra : 1;
+  uint8_t z : 3;
+  uint8_t rcode : 4;
+  uint16_t qdcount : 16;
+  uint16_t ancount : 16;
+  uint16_t nscount : 16;
+  uint16_t arcount : 16;
+}
 
 typedef char* ip_t;
 
